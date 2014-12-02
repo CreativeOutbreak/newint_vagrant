6967f9ff6f32051a6953f832fb0585da78dbf6aa
Pete Stewart <pete@newint.org>
2014-12-02 11:02:45 +0000

    changed legacy_db.sh to check for latest pgdb and if it's not found it then looks for the original dump and then if not found downloads it and imports

--- scripts/ni/legacy_db.sh
+++ scripts/ni/legacy_db.sh
@@ -20,8 +20,8 @@ else
     # If the above is not true, then we need to downloaded it and set it all up.
     echo "Downloading newint2 and then importing it." 
     NI_SQL="$NI_DIR/newint2.sql"
-    wget --username file_admin --password RadicalFish7 http://dev2.newint.org/files/newint2.sql.tar.gz "$NI_DIR/"
-    tar -zxvf "$NI_DIR/newint2.sql.tar.gz"
+    wget --user file_admin --password RadicalFish7 -O "$NI_DIR/newint2.sql.tar.gz" http://dev2.newint.org/files/newint2.sql.tar.gz 
+    tar -zxvf "$NI_DIR/newint2.sql.tar.gz" -C $NI_DIR/
     sudo rm -R "$NI_DIR/newint2.sql.tar.gz"
     cat $NI_DIR/psql.txt | sudo -u postgres psql
     cat $NI_SQL | sudo -u newint2 psql newint2
